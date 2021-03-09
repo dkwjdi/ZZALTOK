5524a5a676d0f20d7b975a96c44cf424d3619dbc
현진혁 <wak8835@gmail.com>
2021-03-09 16:26:31 +0900

    Add S04P22D101-43 : 백엔드 기본 환경 및 데모 코드 작성

---
+++ back/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, File, Form, UploadFile
+
+from typing import Optional, List
+import os
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/files_old/")
+async def create_file(
+    file: UploadFile = File(...)
+):
+    return {
+        "file_name": file.filename,
+        "file_size": len(file.file),
+        "file_content_type": file.content_type,
+    }
+
+@app.post("/files/")
+async def create_files(files: List[bytes] = File(...)):
+    return {"file_sizes": [len(file) for file in files]}
+
+@app.post("/uploadfiles")
+async def create_upload_files(files: List[UploadFile] = File(...)):
+    UPLOAD_DIRECTORY = "./"
+    for file in files:
+        contents = await file.read()
+        with open(os.path.join(UPLOAD_DIRECTORY, file.filename), "wb") as fp:
+            fp.write(contents)
+        print(file.filename)
+    return {"filenames": [file.filename for file in files]}
+
+# @app.post("/files/")
+# async def create_file(
+#     file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)
+# ):
+#     return {
+#         "file_size": len(file),
+#         "token": token,
+#         "fileb_content_type": fileb.content_type,
+#     }
