6a50fe1dfbb4674a71a94113a6d871e5e23321bf
현진혁 <wak8835@gmail.com>
2021-03-11 16:10:33 +0900

    Update S04P22D101-48 : 필요 라이브러리 추가

--- back/run.bat
+++ back/run.bat
@@ -3,6 +3,7 @@ REM 관리자 권한으로 실행하셔야 문제가 없습니다.
 title JJAL Python SERVER
 REM pip freeze > requirements.txt
 set conda=C:\ProgramData\Miniconda3
-%conda%\Scripts\pip.exe install -r requirements.txt
 call %conda%\Scripts\activate.bat %conda%
+call conda install -c conda-forge dlib -y
+call pip install -r requirements.txt
 call uvicorn main:app --reload --host=0.0.0.0 --port=8000
