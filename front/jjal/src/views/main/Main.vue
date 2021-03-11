1fa15da669f5d136e130886160702eab12fde12a
강구원 <fkdldjs487@naver.com>
2021-03-11 16:10:20 +0900

    Add S04P22D101-72 : 공유짤 화면 구성

---
+++ front/jjal/src/views/main/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col :key="n" class="mt-2" cols="10">
+        <strong class="ml-6">Category {{ n }}</strong>
+      </v-col>
+
+      <template>
+        <v-col v-for="j in 4" :key="`${n}${j}`" cols="12" sm="3">
+          <template>
+            <v-row>
+              <v-col cols="1"></v-col>
+              <v-col cols="10">
+                <v-sheet height="180" class="grey lighten-2"></v-sheet>
+              </v-col>
+              <v-col cols="1"></v-col>
+            </v-row>
+          </template>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({ drawer: null }),
+};
+</script>
