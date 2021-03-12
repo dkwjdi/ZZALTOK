432168134057dee9b958e59055d0a18c7233adbc
정준영 <dkwjdiq@naver.com>
2021-03-12 10:56:05 +0900

    Add S04P22D101-77 : 동영상 출력 테스트

--- front/jjal/src/App.vue
+++ front/jjal/src/App.vue
@@ -4,9 +4,6 @@
     <menuBar />
 
     <v-main>
-      <div class="videoContainer">
-        <my-video :sources="video.sources" :options="video.options"></my-video>
-      </div>
       <router-view />
     </v-main>
   </v-app>
@@ -15,39 +12,9 @@
 <script>
 import Navigation from './components/common/Navigation.vue';
 import MenuBar from './components/common/MenuBar.vue';
-import myVideo from 'vue-video';
 
 export default {
   name: 'App',
-  components: { Navigation, MenuBar, myVideo },
-  data() {
-    return {
-      video: {
-        sources: [
-          {
-            src: 'https://www.w3schools.com/tags/movie.mp4',
-            type: 'video/mp4',
-          },
-        ],
-        options: {
-          autoplay: true,
-          controls: true,
-          muted: true,
-          poster: 'http://covteam.u.qiniudn.com/poster.png',
-        },
-      },
-    };
-  },
-
-  created() {},
+  components: { Navigation, MenuBar },
 };
 </script>
-
-<style scoped>
-.videoContainer {
-  width: 50%;
-  min-width: 600px;
-
-  height: 100%;
-}
-</style>
