a10b6548d98ea38f4838e6b1e21a58286a906547
현진혁 <wak8835@gmail.com>
2021-03-14 05:53:53 +0900

    Update S04P22D101-55 : 다메다메 코드 수정, 라이브러리 추가, 라이브러리 설치용 배치파일과 실행 배치파일 분리

--- back/main/damedame.py
+++ back/main/damedame.py
@@ -1,18 +1,18 @@
 import os
+import platform
 import subprocess
 import time
 import git
 import urllib.request
 import zipfile
-
-# 모듈 간 충돌 방지로 인한 OMP 에러 예외처리
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import stat
 
 #작업할 볼륨 또는 폴더 선정
 root = os.path.join(os.path.splitdrive(os.getcwd())[0], "/content/")
 dame_path = os.path.join(root, "damesource/")
 first_order_model_path = os.path.join(root, "first-order-model/")
 video_path = os.path.join(root, "video/")
+ffmpeg_path = os.path.join(root, "ffmpeg/")
 
 if not os.path.isdir(first_order_model_path):
     print("다메다메 프로젝트 클론 중")
@@ -22,7 +22,7 @@ if not os.path.isdir(dame_path):
     print("다메다메 학습 모델 및 템플릿 다운로드 중 - 오래 걸림")
     urllib.request.urlretrieve(
         "https://github.com/KeepSOBP/dame/releases/download/damesource/damesource.zip"
-        , filename= os.path.join(root, "damesource.zip") )
+        , filename= os.path.join(video_path, "damesource.zip") )
     with zipfile.ZipFile(os.path.join(root, "damesource.zip"), "r") as zip_ref:
         zip_ref.extractall(dame_path)
     urllib.request.urlretrieve(
@@ -34,9 +34,25 @@ if not os.path.isdir(dame_path):
 if not os.path.isdir(video_path):
     os.makedirs(video_path, exist_ok=True)
 
+# ffmpeg 파일 다운로드
+if not os.path.isdir(ffmpeg_path):
+    urllib.request.urlretrieve(
+        "http://kumoh.synology.me/ffmpeg.zip"
+        , filename= os.path.join(root, "ffmpeg.zip") )
+    with zipfile.ZipFile(os.path.join(root, "ffmpeg.zip"), "r") as zip_ref:
+        zip_ref.extractall(ffmpeg_path)
+    if platform.system() == "Windows": #윈도우인 경우 linux 실행 파일을 삭제
+        os.remove(os.path.join(ffmpeg_path, "ffmpeg"))
+    else: # 리눅스인 경우 실행 권한 추가
+        st = os.stat(os.path.join(ffmpeg_path, "ffmpeg"))
+        os.chmod(os.path.join(ffmpeg_path, "ffmpeg"), st.st_mode | stat.S_IEXEC)
+
 # 필요 없는 파일 삭제
 if os.path.isfile(os.path.join(root, "complete.mp4")):
   os.remove(os.path.join(root, "complete.mp4"))
+
+if os.path.isfile(os.path.join(root, "ffmpeg.zip")):
+    os.remove(os.path.join(root, "ffmpeg.zip"))
 
 # make damedane
 import sys
@@ -82,9 +98,8 @@ def convertImageToVideo(uploadimage):
         return ani
 
     generator, kp_detector = load_checkpoints(config_path=os.path.join(first_order_model_path, 'config/vox-256.yaml'),
-                                checkpoint_path=os.path.join(dame_path, 'vox-cptork.pth.tar'))
- 
-    # clear_output(wait=True)
+                                checkpoint_path=os.path.join(dame_path, 'vox-cpk.pth.tar'))
+
 
     # make video
     print("영상 제작, 다운로드 진행중")
@@ -92,10 +107,8 @@ def convertImageToVideo(uploadimage):
     filepath = os.path.join(video_path, 'original.mp4')
     imageio.mimsave(filepath, [img_as_ubyte(frame) for frame in predictions])
     return filepath
-    # clear_output(wait=True)
 
 
-import ffmpeg
 # 영상 3배, 음원 삽입 (ffmpeg 이용)
 def insertVideoSoundSource():
     # 필요 없는 파일 삭제
@@ -103,37 +116,25 @@ def insertVideoSoundSource():
         os.remove(os.path.join(video_path, "complete.mp4"))
     print("영상 편집중")
     # !ffmpeg -i original.mp4 -r 60 -vf "setpts=(PTS-STARTPTS)/3" 3x.mp4
-    (
-        ffmpeg
-        .input(os.path.join(video_path, 'original.mp4'))
-        .output()
-        .global_args('-r 60')
-        .global_args('-vf "setpts=(PTS-STARTPTS)/3"')
-        .global_args(os.path.join(video_path, '3x.mp4'))
-        .run()
+    cmd = '{} -i {} -r 60 -vf "setpts=(PTS-STARTPTS)/3" {} -y'.format(
+        os.path.join(ffmpeg_path, 'ffmpeg'),
+        os.path.join(video_path, 'original.mp4'),
+        os.path.join(video_path, '3x.mp4')
     )
+    subprocess.Popen(cmd).wait()
     # !ffmpeg -i 3x.mp4 -i bakamitai_template.mp3 -map 0:v -map 1:a -c:v copy -shortest complete.mp4
-    ffmpeg
-    (
-        ffmpeg
-        .input(os.path.join(video_path, '3x.mp4'))
-        .output('')
-        .global_args('-i {}'.format(os.path.join(dame_path, "bakamitai_template.mp3")))
-        .global_args('-map 0:v')
-        .global_args('-map 1:a')
-        .global_args('-c:v copy')
-        .global_args('-shortest')
-        .global_args(os.path.join(video_path, 'complete.mp4'))
-        .run()
+    cmd = '{} -i {} -i {} -map 0:v -map 1:a -c:v copy -shortest {} -y'.format(
+        os.path.join(ffmpeg_path, 'ffmpeg'),
+        os.path.join(video_path, '3x.mp4'),
+        os.path.join(dame_path, "bakamitai_template.mp3"),
+        os.path.join(video_path, 'complete.mp4')
     )
-    # clear_output(wait=True)
-
-    print("영상 다운중")
-    # files.download('complete.mp4')
+    subprocess.Popen(cmd).wait()
+    print("다메다메 영상 제작 완료")
 
     # os.remove(uploadimage)
-    os.remove("3x.mp4")
-    os.remove("original.mp4")
+    # os.remove("3x.mp4")
+    # os.remove("original.mp4")
 
 if __name__ == "__main__":
     makeDamedame(r"C:\content\damesource\image.png")
