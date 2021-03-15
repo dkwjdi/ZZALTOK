5c79dfbb6101e73d05171afe134a1c76bb7d3f17
강구원 <fkdldjs487@naver.com>
2021-03-15 18:17:28 +0900

    Add S04P22D101-99 : Footer Text 만들었음

--- front/jjal/src/App.vue
+++ front/jjal/src/App.vue
@@ -6,21 +6,28 @@
     <v-main>
       <router-view />
     </v-main>
+
+    <footerBar />
   </v-app>
 </template>
 
 <script>
 import Navigation from './components/common/Navigation.vue';
 import MenuBar from './components/common/MenuBar.vue';
+import FooterBar from './components/common/Footer.vue';
 
 export default {
   name: 'App',
-  components: { Navigation, MenuBar },
+  components: { Navigation, MenuBar, FooterBar },
 };
 </script>
 
 <style>
 .font-we {
   font-weight: 600;
+}
+
+.cur-point {
+  cursor: pointer;
 }
 </style>
