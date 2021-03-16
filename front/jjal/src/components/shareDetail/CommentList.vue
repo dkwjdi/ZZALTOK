f3a7134b18ab3ebb28a1dcc980c21f97be8c0cb1
강구원 <fkdldjs487@naver.com>
2021-03-16 19:50:55 +0900

    Add S04P22D101-109 : 댓글 리스트와 item 제작

---
+++ front/jjal/src/components/shareDetail/CommentList.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row class="mt-1">
+    <v-col cols="1" md="1"></v-col>
+    <v-col cols="12" md="10">
+        <comment-list-item v-for="k in 4" :key=k />
+    </v-col>
+    <v-col cols="1" md="1"></v-col>
+  </v-row>
+</template>
+
+<script>
+import CommentListItem from './CommentListItem.vue'
+export default {
+  components: { CommentListItem },
+}
+</script>
