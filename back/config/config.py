6f92f841406f4627ba47e5b48330474919429e4b
현진혁 <wak8835@gmail.com>
2021-03-22 14:02:09 +0900

    Add S04P23D101-12 : 백엔드 업로드 기능 추가

--- back/config/config.py
+++ back/config/config.py
@@ -3,6 +3,7 @@ import os
 # 작업할 볼륨 또는 폴더 선정
 root: str = os.path.join(os.path.splitdrive(os.getcwd())[0], "/content/")
 image_path: str = os.path.join(root, "images/")
+upload_path: str = os.path.join(root, "uploads/")
 thumbnail_path: str = os.path.join(root, "thumbnails/")
 dame_path: str = os.path.join(root, "damesource/")
 first_order_model_path: str = os.path.join(root, "first-order-model/")
