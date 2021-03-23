211c042083484c92d1f72d75f5dc96abb9066dd8
현진혁 <wak8835@gmail.com>
2021-03-23 10:27:58 +0900

    Add S04P23D101-13 : GPU 서버를 특정할 수 있도록 config 수정

--- back/config/config.py
+++ back/config/config.py
@@ -1,9 +1,12 @@
 import os
 import platform
 
+# GPU 서버여부 체크
+IS_GPU_SERVER: bool = 'gpu' in platform.node() and platform.system() == "Linux"
+
 # 작업할 볼륨 또는 폴더 선정
 root: str = os.path.join(os.path.splitdrive(os.getcwd())[0], "/content/") \
-    if not ('gpu' in platform.node()) else os.path.abspath('content/')
+    if not IS_GPU_SERVER else os.path.abspath('content/')
 image_path: str = os.path.join(root, "images/")
 upload_path: str = os.path.join(root, "uploads/")
 thumbnail_path: str = os.path.join(root, "thumbnails/")
