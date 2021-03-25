3389bc68fdd35aa76f782a40765a1b1f8777742f
DESKTOP-KVCQHCD\multicampus <fkdldjs487@gmail.com>
2021-03-25 12:32:38 +0900

    Updata S04P23D101-14 : Slider 컴포넌트화, 슬라이더 이미지 3개 만듬

--- front/jjal/src/views/main/Main.vue
+++ front/jjal/src/views/main/Main.vue
@@ -1,60 +1,52 @@
 <template>
-  <v-container>
+  <v-container class="main-container" fluid>
     <v-row class="mt-7">
-      <v-col cols="12">
-        <VueSlickCarousel v-bind="slickData">
-          <!-- 페이지 이동권 
-            @click="movePage('DeepFakeMovie')"
-            가볍게 넘겼는데도 클릭으로 이동함, 특정화면 클릭시 or 이동 편하게 멀 만들어주든가.
-           -->
-          <v-img
-            :src="require('../../assets/fakeImage_banner.png')"
-            aspect-ratio="6"
-            @click="movePage('DeepFakeMovie')"
-          ></v-img>
-          <v-img :src="require('../../assets/fakemovie_banner.png')" aspect-ratio="6"></v-img>
-          <v-img :src="require('../../assets/example.png')" aspect-ratio="6"></v-img>
-        </VueSlickCarousel>
-      </v-col>
+      <slider />
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="item in getShareItems"
+            :key="item.board_no"
+            cols="12"
+            sm="6"
+            md="4"
+            xl="3"
+          >
+            <share-list-item
+              :board_no="item.board_no"
+              :title="item.title"
+              :content="item.content"
+              :contentType="item.contentType"
+              :ip="item.ip"
+              :good="item.good"
+              :regdate="item.regdate"
+              :imageUrl="'http://localhost:8000' + JSON.parse(item.content).url"
+              :video="'hi'"
+            />
+          </v-col>
 
-      <!-- <v-col :key="n" class="mt-2" cols="12">
-        <div style="font-weight: bolder; text-align: center">
-          <strong class="text-xl-h4 text-lg-h6">공유짤</strong>
-        </div>
-      </v-col> -->
-
-      <v-col v-for="item in getShareItems" :key="item.board_no" cols="12" sm="6" md="4" xl="3">
-        <share-list-item 
-        :board_no="item.board_no"
-        :title="item.title"
-        :content="item.content"
-        :contentType="item.contentType"
-        :ip="item.ip"
-        :good="item.good"
-        :regdate="item.regdate"
-        :imageUrl="'http://localhost:8000'+JSON.parse(item.content).url" 
-        :video="'hi'"/>
-      </v-col>
-
-      <v-col cols="12">
-        <div style="text-align: center">
-          <v-btn text @click="more()">The More</v-btn>
-        </div>
-      </v-col>
+          <v-col cols="12">
+            <div style="text-align: center">
+              <v-btn text @click="more()">The More</v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import VueSlickCarousel from 'vue-slick-carousel';
-import 'vue-slick-carousel/dist/vue-slick-carousel.css';
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
-import ShareListItem from '../../components/main/ShareListItem.vue';
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+import ShareListItem from "../../components/main/ShareListItem.vue";
 import { mapGetters, mapActions } from "vuex";
+import Slider from "../../components/main/Slider.vue";
 
 export default {
-  components: { VueSlickCarousel, ShareListItem },
-  computed:{
+  components: { VueSlickCarousel, ShareListItem, Slider },
+  computed: {
     ...mapGetters("mainStore", ["getShareItems"]),
   },
   data: () => ({
@@ -76,8 +68,8 @@ export default {
 
     items: {},
   }),
-methods: {
-    ...mapActions("mainStore",["fetchShareList"]),
+  methods: {
+    ...mapActions("mainStore", ["fetchShareList"]),
     movePage: function (move) {
       this.$router.push({ name: move });
     },
@@ -93,3 +85,9 @@ methods: {
   },
 };
 </script>
+
+<style>
+.main-container {
+  margin-top: -40px;
+}
+</style>
