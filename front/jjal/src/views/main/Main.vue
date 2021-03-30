fb5d91724af5da475bec47ff541c6926c1987363
강구원 <fkdldjs487@naver.com>
2021-03-30 13:17:10 +0900

    Update S04P23D101-104, S04P23D101-108 : 메인페이지 UI/UX 수정, 반응형 적용

--- front/jjal/src/views/main/Main.vue
+++ front/jjal/src/views/main/Main.vue
@@ -1,47 +1,138 @@
 <template>
-  <v-container class="main-container back-img" fluid>
-    <v-row class="mt-7">
-      <slider />
-      <v-container>
-        <v-row>
-          <v-col cols="12">
-            <div class="mt-4 ml-5">
-              <list-tab />
+  <v-container style="padding: 0px; margin-top: -92px" fluid>
+    <v-row>
+      <v-col cols="12">
+        <div class="section share-img">
+          <div class="share-text-body">
+            <div data-aos="fade-up" data-aos-duration="2000" class="share-text">
+              <div class="text-xl-h3 text-md-h4 text-h6">
+                <span>나만의 밈 공유</span>
+              </div>
+              <div class="mt-3">
+                <span>내가 만든 밈을 공유하고 사람들의 밈을 확인해보세요!</span>
+              </div>
+              <div>
+                <v-btn class="mt-3" outlined dark large>바로가기</v-btn>
+              </div>
             </div>
-            <v-divider></v-divider>
-          </v-col>
-          <v-col
-            v-for="item in getShareItems"
-            :key="item.board_no"
-            cols="12"
-            sm="6"
-            md="4"
-            xl="3"
-          >
-            <share-list-item
-              :board_no="item.board_no"
-              :title="item.title"
-              :content="item.content"
-              :contentType="item.contentType"
-              :ip="item.ip"
-              :good="item.good"
-              :regdate="timeForToday(item.regdate)"
-              :imageUrl="getUrl(item.content)"
-              :thumbnail="item.thumbnail"
-              :video="'hi'"
-              :view_cnt="item.view_cnt"
-            />
-          </v-col>
+          </div>
+        </div>
+      </v-col>
 
-          <v-col cols="12">
-            <div style="text-align: center">
-              <v-btn text @click="more()" class="mb-5 mt-5"
-                ><strong>The More</strong></v-btn
-              >
+      <v-col cols="6" class="hidden-md-and-down">
+        <div data-aos="zoom-in" data-aos-duration="2000" class="main-text">
+          <div class="text-xl-h3 text-md-h4 text-h6 height-medi">
+            <span>얼굴 체인지</span>
+          </div>
+          <i class="far fa-laugh-squint"></i>
+          <div class="mt-3">원하는 인물과 자신의 사진을 합성해보세요!</div>
+          <v-btn class="mt-3" rounded outlined large color="#ff5757"
+            >바로가기</v-btn
+          >
+        </div>
+      </v-col>
+
+      <v-col cols="12" lg="6">
+        <div data-aos="fade-up" data-aos-duration="2000" class="mr-5">
+          <v-img
+            :src="require('../../assets/1.png')"
+            class=""
+            width="1260px"
+            height="950px"
+            style="float: right"
+            ><div class="main-img-text hidden-md-and-up">
+              <div class="text-xl-h3 text-md-h4 text-h6">
+                <span>얼굴 체인지</span>
+              </div>
+              <div class="mt-3">
+                <span>원하는 인물과 자신의 사진을 합성해보세요!</span>
+              </div>
+              <div>
+                <v-btn class="mt-3" rounded outlined dark large>바로가기</v-btn>
+              </div>
             </div>
-          </v-col>
-        </v-row>
-      </v-container>
+          </v-img>
+        </div>
+      </v-col>
+
+      <v-col cols="12" lg="6">
+        <div
+          data-aos="fade-right"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="2000"
+          class="ml-5"
+        >
+          <v-img
+            :src="require('../../assets/2.png')"
+            class=""
+            width="1260px"
+            height="950px"
+            ><div class="main-img-text hidden-md-and-up">
+              <div class="text-xl-h3 text-md-h4 text-h6">
+                <span>다메다메 영상</span>
+              </div>
+              <div class="mt-3">
+                <span
+                  >핫한 다메다메 아저씨 영상에 자신의 얼굴을 넣어보세요!</span
+                >
+              </div>
+              <div>
+                <v-btn class="mt-3" rounded outlined dark large>바로가기</v-btn>
+              </div>
+            </div></v-img
+          >
+        </div>
+      </v-col>
+      <v-col cols="6" class="hidden-md-and-down">
+        <div
+          data-aos="zoom-in-down"
+          data-aos-duration="2000"
+          class="main-text2"
+        >
+          <div class="text-xl-h3 text-md-h4 text-h6 height-medi">
+            <span>다메다메 영상</span>
+          </div>
+          <div class="mt-3">
+            핫한 다메다메 아저씨 영상에 자신의 얼굴을 넣어보세요!
+          </div>
+          <v-btn class="mt-3" rounded outlined large color="#d4bdad"
+            >바로가기</v-btn
+          >
+        </div>
+      </v-col>
+
+      <v-col cols="6" class="hidden-md-and-down">
+        <div data-aos="flip-left" data-aos-duration="2000" class="main-text3">
+          <div class="text-xl-h3 text-md-h4 text-h6 height-medi">
+            <span>나만의 배경</span>
+          </div>
+          <div class="mt-3">원하는 동영상에 배경을 바꾸고 싶다면?</div>
+          <v-btn class="mt-3" rounded outlined large color="#fce9b5"
+            >바로가기</v-btn
+          >
+        </div>
+      </v-col>
+
+      <v-col cols="12" lg="6">
+        <div data-aos="fade-left" data-aos-duration="2000" class="mr-5">
+          <v-img
+            :src="require('../../assets/3.png')"
+            width="1260px"
+            height="950px"
+            ><div class="main-img-text hidden-md-and-up">
+              <div class="text-xl-h3 text-md-h4 text-h6">
+                <span>나만의 배경</span>
+              </div>
+              <div class="mt-3">
+                <span>원하는 동영상에 배경을 바꾸고 싶다면?</span>
+              </div>
+              <div>
+                <v-btn class="mt-3" rounded outlined dark large>바로가기</v-btn>
+              </div>
+            </div></v-img
+          >
+        </div>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -54,6 +145,7 @@ import ShareListItem from "../../components/main/ShareListItem.vue";
 import { mapGetters, mapActions } from "vuex";
 import Slider from "../../components/main/Slider.vue";
 import ListTab from "../../components/main/ListTab.vue";
+import $ from "jquery";
 
 export default {
   components: { VueSlickCarousel, ShareListItem, Slider, ListTab },
@@ -84,45 +176,17 @@ export default {
     movePage: function (move) {
       this.$router.push({ name: move });
     },
-    more() {
-      this.$store.commit("mainStore/SET_PAGE_COUNT", "more");
-      this.fetchShareList(this.getCurrentTab);
-    },
-
-    getUrl(str){
-      return JSON.parse(str).url
-    },
-
-    timeForToday(value) {
-      const today = new Date();
-      const timeValue = new Date(value);
-
-      const betweenTime = Math.floor(
-        (today.getTime() - timeValue.getTime()) / 1000 / 60
-      );
-      if (betweenTime < 1) return "방금전";
-      if (betweenTime < 60) {
-        return `${betweenTime}분전`;
-      }
-
-      const betweenTimeHour = Math.floor(betweenTime / 60);
-      if (betweenTimeHour < 24) {
-        return `${betweenTimeHour}시간전`;
-      }
-
-      const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
-        if (betweenTimeDay < 365) {
-            return `${betweenTimeDay}일전`;
-        }
-    },
   },
 
   created() {
     window.scrollTo(0, 0);
-    //axios하기
-    this.$store.commit("mainStore/SET_PAGE_COUNT", "first");
-    this.$store.commit("mainStore/SET_SHARE_ITEMS_RESET");
-    this.fetchShareList('good');
+
+    // appbar 관리
+    console.log("여기???");
+    $("#nav-ul-id").addClass("main-bar");
+    $("#nav-ul-id").removeClass("func-bar");
+    $("#navbar").css("background-color", "rgba( 255, 255, 255, 0 )");
+    $(".nav_ul").css("color", "white");
   },
 };
 </script>
@@ -134,5 +198,64 @@ export default {
 
 .back-img {
   background-color: rgb(249, 249, 249);
+}
+
+.section {
+  height: 1000px;
+  width: 100vw;
+  display: table;
+  table-layout: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
+.share-img {
+  background-image: url("../../assets/share.png");
+}
+
+.share-text {
+  color: white;
+  /* transform: translate(-40%, -50%); */
+  text-align: center;
+}
+
+.share-text-body {
+  position: absolute;
+  top: 7%;
+  text-align: center;
+  width: 100vw;
+}
+
+.main-text {
+  text-align: center;
+  color: #ff5757;
+  height: 1000px;
+  font-weight: bold;
+}
+
+.main-text2 {
+  text-align: center;
+  color: #d4bdad;
+  height: 1000px;
+  font-weight: bold;
+}
+
+.main-text3 {
+  text-align: center;
+  color: #fce9b5;
+  height: 1000px;
+  font-weight: bold;
+}
+
+.height-medi {
+  height: 480px;
+  padding-top: 430px;
+}
+
+.main-img-text {
+  color: white;
+  text-align: center;
+  height: 1000px;
+  padding-top: 400px;
 }
 </style>
