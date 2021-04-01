8d46cb117dddb1ced943dacffea9e3cba68f8f39
현진혁 <wak8835@gmail.com>
2021-04-01 09:29:09 +0900

    Fix S04P23D101-124 : 조회 시 mysql connector cursor에 의한 UnboundLocalError 수정

--- back/utils/db.py
+++ back/utils/db.py
@@ -13,10 +13,9 @@ if not config.IS_GPU_SERVER:
         database="jjal",
         autocommit=True
     )
-    # cursor = database.cursor()
 else:
     database = None
-    # cursor = None
+
 
 # print(mydb)
 
@@ -30,8 +29,7 @@ async def check_password_on_board(
     # 해당 게시물 비밀번호 얻어오기
     sql = "SELECT password FROM board WHERE board_no = %s"
 
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, (board_no,))
         res = cursor.fetchone()
         for pwd in res:
@@ -40,13 +38,6 @@ async def check_password_on_board(
             else:
                 return False
 
-    except Error as e:
-        print(e)
-        return None
-
-    finally:
-        cursor.close()
-
 
 async def check_password_on_comment(
         password: str,
@@ -54,8 +45,7 @@ async def check_password_on_comment(
 ):
     # 해당 게시물 비밀번호 얻어오기
     sql = "SELECT password FROM comment WHERE comment_no = %s"
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, (comment_no,))
         res = cursor.fetchone()
         for pwd in res:
@@ -63,12 +53,6 @@ async def check_password_on_comment(
                 return True
             else:
                 return False
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 async def check_user_ip_on_good_list(
@@ -80,20 +64,13 @@ async def check_user_ip_on_good_list(
             SELECT IP FROM goodlist 
             WHERE board_no = %s AND ip = %s
           """
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, (board_no, ip))
         res = cursor.fetchone()
         if res is None:
             return False
         else:
             return True
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 async def insert_user_ip_on_good_list(
@@ -105,17 +82,10 @@ async def insert_user_ip_on_good_list(
             VALUES (%s, %s)
           """
     val = (board_no, ip)
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return True
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 async def delete_user_ip_on_good_list(
@@ -127,17 +97,11 @@ async def delete_user_ip_on_good_list(
             WHERE board_no = %s AND ip = %s;
           """
     val = (board_no, ip)
-    try:
-        cursor = database.cursor()
+
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return True
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 # 여기까지 공용 기능 종료 ###
@@ -150,63 +114,46 @@ async def delete_user_ip_on_good_list(
 async def increase_view_count(
         board_no: int
 ):
-    cursor = database.cursor()
     sql = """
            UPDATE board 
               SET view_cnt = view_cnt+1
             WHERE board_no = %s
           """
     val = (board_no,)
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return "게시글 조회 증가 성공"
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 # S04P23D101-91      백엔드 RESTful API 프로토콜 / 게시글 조회수 감소
 async def decrease_view_count(
         board_no: int
 ):
-    cursor = database.cursor()
     sql = """
            UPDATE board 
               SET view_cnt = view_cnt-1
             WHERE board_no = %s
           """
     val = (board_no,)
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return "게시글 조회 감소 성공"
 
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
-
 
 #  S04P23D101-71        백엔드 RESTful API 프로토콜 / 최근 게시글들 조회(추천순 반영 상위 12개)
 async def find_all_board_on_day_by_newest(
-    page_count: int
+        page_count: int
 ):
-    try:
-        cursor = database.cursor()
-        sql = """
-                SELECT * 
-                  FROM board b
-                 ORDER BY board_no DESC
-                 LIMIT %s, 12;
-               """
-        page_count = (page_count-1) * 12
+    sql = """
+            SELECT * 
+              FROM board b
+             ORDER BY board_no DESC
+             LIMIT %s, 12;
+           """
+    with database.cursor() as cursor:
+        page_count = (page_count - 1) * 12
         cursor.execute(sql, (page_count,))
         res = cursor.fetchall()
         result = []
@@ -235,26 +182,20 @@ async def find_all_board_on_day_by_newest(
                            'view_cnt': item[9]
                            })
         return result
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-63	백엔드 RESTful API 프로토콜 / 최근 게시글들 조회(추천순 반영 상위 12개)
 async def find_all_board_on_day_by_good(
-    page_count: int
+        page_count: int
 ):
-    try:
-        cursor = database.cursor()
-        sql = """
-                SELECT * 
-                  FROM board b
-                 ORDER BY good DESC
-                 LIMIT %s, 12;
-               """
-        page_count = (page_count-1) * 12
+    sql = """
+            SELECT * 
+              FROM board b
+             ORDER BY good DESC
+             LIMIT %s, 12;
+           """
+    with database.cursor() as cursor:
+        page_count = (page_count - 1) * 12
         cursor.execute(sql, (page_count,))
         res = cursor.fetchall()
         result = []
@@ -283,26 +224,20 @@ async def find_all_board_on_day_by_good(
                            'view_cnt': item[9]
                            })
         return result
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-63	백엔드 RESTful API 프로토콜 / 최근 게시글들 조회(추천순 반영 상위 12개)
 async def find_all_board_on_day_by_view(
-    page_count: int
+        page_count: int
 ):
-    try:
-        cursor = database.cursor()
-        sql = """
-                SELECT * 
-                  FROM board b
-                 ORDER BY view_cnt DESC
-                 LIMIT %s, 12;
-               """
-        page_count = (page_count-1) * 12
+    sql = """
+            SELECT * 
+              FROM board b
+             ORDER BY view_cnt DESC
+             LIMIT %s, 12;
+           """
+    with database.cursor() as cursor:
+        page_count = (page_count - 1) * 12
         cursor.execute(sql, (page_count,))
         res = cursor.fetchall()
         result = []
@@ -331,11 +266,6 @@ async def find_all_board_on_day_by_view(
                            'view_cnt': item[9]
                            })
         return result
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-64     백엔드 RESTful API 프로토콜 / 게시글 상세 조회
@@ -344,8 +274,7 @@ async def find_board_detail_by_board_no(
 ):
     sql = "SELECT * FROM board WHERE board_no = %s"
 
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, (board_no,))
         res = cursor.fetchone()
         if res is None:
@@ -375,11 +304,6 @@ async def find_board_detail_by_board_no(
                 'regdate': res[8],
                 'view_cnt': res[9]
                 }
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-57	백엔드 RESTful API 프로토콜 / 게시글 작성(공유)
@@ -393,19 +317,12 @@ async def write_board(
           """
     val = (board_info['title'], board_info['content'], board_info['content_type'],
            board_info['nickname'], board_info['password'], board_info['ip'], dt.datetime.now())
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         board_no = cursor.lastrowid
         return {"message": "게시물 작성에 성공했습니다.",
                 "board_no": board_no}
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-67     백엔드 RESTful API 프로토콜 / 게시글 수정
@@ -422,16 +339,10 @@ async def edit_board(
     val = (board_info['title'], board_info['content'], board_info['content_type'],
            board_info['nickname'], board_info['password'],
            board_info['ip'], board_info['board_no'])
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return "게시물 수정에 성공했습니다."
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-60	백엔드 RESTful API 프로토콜 / 게시글 삭제
@@ -441,16 +352,11 @@ async def delete_board(
     # 비밀번호 ??
     sql = "DELETE FROM board WHERE board_no = %s"
     val = (board_no,)
-    try:
-        cursor = database.cursor()
+
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return "게시물 삭제에 성공했습니다."
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-62	백엔드 RESTful API 프로토콜 / 게시글 추천(좋아요 기능)
@@ -463,17 +369,11 @@ async def count_up_thumbs_up_on_board(
                 WHERE board_no = %s
           """
     val = (board_no,)
-    try:
-        cursor = database.cursor()
+
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return "좋아요!"
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-62	백엔드 RESTful API 프로토콜 / 게시글 추천(좋아요 기능)
@@ -486,17 +386,11 @@ async def count_down_thumbs_up_on_board(
                 WHERE board_no = %s
           """
     val = (board_no,)
-    try:
-        cursor = database.cursor()
+
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return True
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P23D101-37 백엔드 RESTful API 프로토콜 / 게시글 공유
@@ -510,19 +404,12 @@ async def write_share_board(
           """
     val = (board_info['content'], board_info['content_type'],
            board_info['nickname'], board_info['ip'], dt.datetime.now())
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         s_board_no = cursor.lastrowid
         return {"message": "공유 게시물 작성에 성공했습니다.",
                 "s_board_no": s_board_no}
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 async def find_share_board_detail_by_board_no(
@@ -530,8 +417,7 @@ async def find_share_board_detail_by_board_no(
 ):
     sql = "SELECT * FROM shareboard WHERE s_board_no = %s"
 
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, (s_board_no,))
         res = cursor.fetchone()
         if res is None:
@@ -557,11 +443,6 @@ async def find_share_board_detail_by_board_no(
                 'ip': ip,
                 'regdate': res[5]
                 }
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 # 여기까지 게시글 기능 종료 ###
@@ -580,8 +461,7 @@ async def find_comment_by_board_no(
              WHERE board_no = %s
           """
     val = (board_no,)
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         res = cursor.fetchall()
         result = []
@@ -609,12 +489,6 @@ async def find_comment_by_board_no(
                            })
         return result
 
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
-
 
 #  S04P22D101-59	백엔드 RESTful API 프로토콜 / 댓글 작성
 async def write_comment(
@@ -627,17 +501,10 @@ async def write_comment(
     val = (comment_info['board_no'], comment_info['content'],
            comment_info['nickname'], comment_info['password'],
            comment_info['ip'], dt.datetime.now())
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return "댓글 작성에 성공했습니다."
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-61	백엔드 RESTful API 프로토콜 / 댓글 삭제
@@ -650,17 +517,10 @@ async def delete_comment(
             WHERE comment_no = %s
           """
     val = (comment_no,)
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return "댓글 삭제에 성공했습니다."
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 
 #  S04P22D101-66     백엔드 RESTful API 프로토콜 / 댓글 수정
@@ -675,16 +535,9 @@ async def edit_comment(
     val = (comment_info['content'], comment_info['nickname'],
            comment_info['password'], comment_info['ip'],
            comment_info['comment_no'])
-    try:
-        cursor = database.cursor()
+    with database.cursor() as cursor:
         cursor.execute(sql, val)
         database.commit()
         return "댓글 수정에 성공했습니다."
-
-    except Error as e:
-        print(e)
-        return None
-    finally:
-        cursor.close()
 
 # 여기까지 댓글 기능 종료 ###
